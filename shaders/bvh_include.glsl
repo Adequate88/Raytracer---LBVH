3e95b0cc51ee0ea5a7665354c8c558e8a4ed911c
mmmuscus <mmmuscus@gmail.com>
2026-06-17 14:03:11 +0200

    aabb_hit on GPU

--- shaders/bvh_include.glsl
+++ shaders/bvh_include.glsl
@@ -20,3 +20,19 @@ layout(binding = 0, std430) readonly buffer SceneBuffer {
 layout(binding = 1, std430) buffer BvhBuffer {
   node nodes[];
 } bvh;
+
+bool aabb_hit(vec3 ray_origin, vec3 ray_dir, float t_curr_min,
+              vec3 aabb_min, vec3 aabb_max) {
+  vec3 adinv = 1.0f / ray_dir; // TODO: replace with precomputed argument
+
+  vec3 t0 = (aabb_min - ray_origin) * adinv;
+  vec3 t1 = (aabb_max - ray_origin) * adinv;
+
+  vec3 t_min_vec = min(t0, t1);
+  vec3 t_max_vec = max(t0, t1);
+
+  float t_min = min(min(t_min_vec.x, t_min_vec.y), t_min_vec.z);
+  float t_max = max(max(t_max_vec.x, t_max_vec.y), t_max_vec.z);
+
+  return (t_min <= t_max) && (t_max > 0.0) && (t_min < t_curr_min);
+}
